d7af8fcb177882580a0b4b104e214cf860f9e34f
yasmin brown <yasminbrown6@hotmail.com>
2022-11-15 13:02:19 +0000

    completed back end for task manager rails challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :tasks
+  # get '/tasks', to: "tasks#index"
+  # get '/tasks/new', to: "tasks#new"
+  # post '/tasks', to: "tasks#create"
+  # get '/tasks/:id', to: "tasks#show", as: :task
+  # delete '/tasks/:id', to: "tasks#destroy"
+  # get '/tasks/:id/edit', to: "tasks#edit", as: :edit_task
+  # patch '/tasks/:id', to: "tasks#update"
 end
